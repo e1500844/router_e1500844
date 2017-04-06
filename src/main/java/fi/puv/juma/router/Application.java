3583e4aead6e62b39b77040bf359c632114e758d
e1600538 <quentin.flueckiger@hispeed.ch>
2017-04-06 15:10:49 +0300

    poistettu turhat kommentit, lisätty toiminto

--- src/main/java/fi/puv/juma/router/Application.java
+++ src/main/java/fi/puv/juma/router/Application.java
@@ -31,51 +31,5 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class);
 	}
-
-
-	@Bean
-	public CommandLineRunner demo(WifiRepository repository) {
-		return (args) -> {
-			Wifi w = new Wifi();
-			w.setCrypt(1);
-			Instant instant = Instant.now(); //fix timezone
-			ZoneId zoneId = ZoneId.of("Europe/Helsinki");
-			ZonedDateTime zdt = ZonedDateTime.ofInstant( instant , zoneId );
-			w.setTimestamp(new Timestamp(zdt.toInstant().getEpochSecond() * 1000L));	  			
-			repository.save(w);
-			
-			
-			/*			// save a couple of customers
-			repository.save(new Customer("Jack", "Bauer"));
-			repository.save(new Customer("Chloe", "O'Brian"));
-			repository.save(new Customer("Kim", "Bauer"));
-			repository.save(new Customer("David", "Palmer"));
-			repository.save(new Customer("Michelle", "Dessler"));
-
-			// fetch all customers
-			log.info("Customers found with findAll():");
-			log.info("-------------------------------");
-			for (Customer customer : repository.findAll()) {
-				log.info(customer.toString());
-			}
-			log.info("");
-
-			// fetch an individual customer by ID
-			Customer customer = repository.findOne(1L);
-			log.info("Customer found with findOne(1L):");
-			log.info("--------------------------------");
-			log.info(customer.toString());
-			log.info("");
-
-			// fetch customers by last name
-			log.info("Customer found with findByLastName('Bauer'):");
-			log.info("--------------------------------------------");
-			for (Customer bauer : repository.findByLastName("Bauer")) {
-				log.info(bauer.toString());
-			}
-			log.info("");
-			*/
-		};
-	}
 	
 }
